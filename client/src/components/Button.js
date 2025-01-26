5b51a2e4821b053a5b13983823f131a5ce60a692
KavyashreeBaburaj <kavyashreebaburaj@gmail.com>
2025-01-26 17:02:27 +0530

    Logics added for admin, librarian and patron

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,25 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function Button({
-    title,
-    variant = "contained",
-    color = "primary",
-    type ='button',
-    onClick
+  title,
+  variant = "contained",
+  color = "primary",
+  type = "button",
+  onClick,
+  fullWidth = false,
+  disabled
 }) {
+  let className = fullWidth ? "w-100 rounded " : "pr-2 pl-2 rounded ";
+  if (variant === "contained" && !disabled) {
+    className += "bg-" + color + " text-white";
+  } else if (variant === "outlined" && !disabled) {
+    className += "border-" + color + " text-" + color;
+  }
 
-let className= "w-100 pt-1 pb-1 "
-if(variant === "contained"){
-    className+= "bg-" +color + " text-white";
-}else if(variant === "outlined"){
-    className +='border-'+color+' text-'+color;
-}
+  if (disabled) {
+    className += "disabled-btn";
+  }
 
   return (
     <button className={className} type={type} onClick={onClick}>
-        {title}
+      {title}
     </button>
-  )
+  );
 }
 
-export default Button
+export default Button;
--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,48 +1,97 @@
-import React from 'react'
-import { Form, message } from 'antd';
-import Button from '../../components/Button';
-import { LoginUser } from '../../apicalls/users';
+import React, { useEffect } from "react";
+import { Form, message, Alert } from "antd";
+import Button from "../../components/Button";
+import { Link, useNavigate } from "react-router-dom";
+import { LoginUser } from "../../apicalls/users";
+import { useDispatch } from "react-redux";
+import { HideLoading, ShowLoading } from "../../redux/loadersSlice";
 
 function Login() {
-  const onFinish = async(values) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [error, setError] = React.useState(null); // State to track error
+
+  const onFinish = async (values) => {
     try {
+      dispatch(ShowLoading());
       const response = await LoginUser(values);
-      if(response.success){
+      dispatch(HideLoading());
+      if (response.success) {
         message.success(response.message);
-        alert(`Success: ${response.message}`);
-        localStorage.setItem("token",response.data);
-        window.location.href="/";
-      }else{
+        localStorage.setItem("token", response.data);
+        window.location.href = "/";
+      } else {
+        setError(response.message); // Set error message
         message.error(response.message);
-        alert(`Error: ${response.message}`);
       }
     } catch (error) {
+      dispatch(HideLoading());
+      setError(error.message); // Set error message
       message.error(error.message);
-      alert(`Error: ${error.message}`);
     }
   };
 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      navigate("/");
+    }
+  }, [navigate]);
+
   return (
-    <div className="h-screen bg-primary flex items-center justify-center ">
-      <div className="authentication-form bg-white p-2">
-        <h1 className='text-secondary text-2xl font-bold justify-center '>
-            BOOKIFY - LOGIN
+    <div className="h-screen bg-primary flex items-center justify-center relative">
+      {error && (
+        <Alert
+          message="Error"
+          description={error}
+          type="error"
+          showIcon
+          style={{
+            position: "absolute",
+            top: "10px",
+            left: "50%",
+            transform: "translateX(-50%)",
+            zIndex: 10,
+          }}
+        />
+      )}
+      <div className="authentication-form bg-white p-3 rounded w-full sm:w-96">
+        <h1 className="text-secondary text-2xl font-bold mb-1">
+          BOOKIFY - LOGIN
         </h1>
-        <hr/>
-        <Form layout="vertical" onFinish={onFinish} className='mt-1'>
-          {/* <Form.Item label="Name" name="name">
-            <input type="text" placeholder="Name" />
-          </Form.Item> */}
-          <Form.Item label="Email" name="email">
+        <hr />
+        
+        <Form layout="vertical" onFinish={onFinish}>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Please input your email!",
+              },
+            ]}
+          >
             <input type="email" placeholder="Email" />
           </Form.Item>
-          <Form.Item label="Password" name="password">
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
             <input type="password" placeholder="Password" />
           </Form.Item>
-          
-          <div className='text-center mt-2 flex-col gap-1'>
-          <Button title="Login" type="submit" color="primary"/>
-            <a href='/register' className='text-primary text-sm underline '>Dont have an account? Click Here To Register.</a>
+
+          <div className="text-center mt-2 flex flex-col gap-1">
+            <Button title="Login" type="submit" />
+            <Link to="/register" className="text-primary text-sm underline">
+              Don’t have an account? Click Here To Register
+            </Link>
           </div>
         </Form>
       </div>
